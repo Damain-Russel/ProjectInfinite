9ea26dc715705257b0150c4d2bd89f687ab07722
Milton Edwards <Damain-Russel@users.noreply.github.com>
2015-09-12 08:01:48 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<!--
+/**
+  * @author Milton Edwards
+  * @Copyright © 2015 InfiniteDesigns 
+  * <https://www.facebook.com/YourBoiGeo>
+  * Please retain this author and license notice!
+  */
+-->
+<?php include("./inc/login.header.inc.php"); ?>
+
+
+<div class="content">
+    <h1>Welcome to ProjectInfinite</h1> 
+    <p>Your place to socialise with <strong>Friends</strong></p>
+    <hr id="one">
+</div>
+<div id="connect">
+    <div class="connect">
+        <h3>Want to Stay Connected With Your Friends?</h3><br /><br /><br /><br />
+        <button type="button" id="new">Create a Free Account</button><br /><br />
+        <button type="button" id="fb">Sign Up With Facebook</button><br /><br />
+        <button type="button" id="goo">Sign Up With Google+</button><br /><br />
+    </div>
+</div>
+<div id="band">
+    <a href="#">
+        <div class="inner" id="info">
+        </div>
+    </a>
+    <a href="#">
+        <div class="inner" id="ads">
+        </div>
+    </a>
+    <a href="#">
+        <div class="inner" id="mobile">
+        </div>
+    </a>
+</div>
+<hr id="two">
+<?php include("./inc/footer.inc.php"); ?>
+<div id="smtn">
+</div>
